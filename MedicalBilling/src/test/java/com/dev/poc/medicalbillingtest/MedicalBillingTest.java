7689f77362dfca568b315eb56fc5929657f7ded1
yogeshabc12 <yogesh.chin@gmail.com>
2017-01-09 15:33:33 +0000

    TDD Test case 2 Added new MediHealth Service - 0.2.0
    added one more TDD test for calculating total cost of two MediHealth
    Services

--- MedicalBilling/src/test/java/com/dev/poc/medicalbillingtest/MedicalBillingTest.java
+++ MedicalBilling/src/test/java/com/dev/poc/medicalbillingtest/MedicalBillingTest.java
@@ -18,5 +18,14 @@ public class MedicalBillingTest {
 		shoppingCart.accept(new GenerateMedicalBilling());
 		assertEquals("Total Billing Cost",shoppingCart.getTotalBill(), 60.00, 0);
 	}
+	
+	@Test
+	public void whenTwoMediHealthServiceThanTotalCost() throws Exception {
+		ShoppingCart shoppingCart = new ShoppingCart();
+		shoppingCart.addCartItem(new CartItem(MediHealthEnum.DAGNOSIS.name(), 60.00, 1));
+		shoppingCart.addCartItem(new CartItem(MediHealthEnum.X_RAY.name(), 150.00, 1));
+		shoppingCart.accept(new GenerateMedicalBilling());
+		assertEquals("Total Billing Cost",shoppingCart.getTotalBill(), 210.00, 0);
+	}
 
 }
