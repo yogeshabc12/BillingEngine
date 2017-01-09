78572c5e2cae8f92b354f4b4e57c5d7927332d4d
yogeshabc12 <yogesh.chin@gmail.com>
2017-01-09 15:39:26 +0000

    TDD Test case calculate total for other medial devices - 0.1.4
    TDD for getting total cost of all the medi helath services

--- MedicalBilling/src/test/java/com/dev/poc/medicalbillingtest/MedicalBillingTest.java
+++ MedicalBilling/src/test/java/com/dev/poc/medicalbillingtest/MedicalBillingTest.java
@@ -27,5 +27,26 @@ public class MedicalBillingTest {
 		shoppingCart.accept(new GenerateMedicalBilling());
 		assertEquals("Total Billing Cost",shoppingCart.getTotalBill(), 210.00, 0);
 	}
+	
+	@Test
+	public void whenThreeMediHealthServiceThanTotalCost() throws Exception {
+		ShoppingCart shoppingCart = new ShoppingCart();
+		shoppingCart.addCartItem(new CartItem(MediHealthEnum.DAGNOSIS.name(), 60.00, 1));
+		shoppingCart.addCartItem(new CartItem(MediHealthEnum.X_RAY.name(), 150.00, 1));
+		shoppingCart.addCartItem(new CartItem(MediHealthEnum.ECG.name(), 200.40, 1));
+		shoppingCart.accept(new GenerateMedicalBilling());
+		assertEquals("Total Billing Cost",shoppingCart.getTotalBill(), 410.40, 0);
+	}
+	
+	@Test
+	public void whenAllMediHealthServiceThanTotalCost() throws Exception {
+		ShoppingCart shoppingCart = new ShoppingCart();
+		shoppingCart.addCartItem(new CartItem(MediHealthEnum.DAGNOSIS.name(), 60.00, 1));
+		shoppingCart.addCartItem(new CartItem(MediHealthEnum.X_RAY.name(), 150.00, 1));
+		shoppingCart.addCartItem(new CartItem(MediHealthEnum.ECG.name(), 200.40, 1));
+		shoppingCart.addCartItem(new CartItem(MediHealthEnum.BLOOD_TEST.name(), 78.00, 1));
+		shoppingCart.accept(new GenerateMedicalBilling());
+		assertEquals("Total Billing Cost",shoppingCart.getTotalBill(), 488.4, 0);
+	}
 
 }
