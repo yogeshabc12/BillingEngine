4636ca7bda9b29005f3c93e44e50b24a30731168
yogeshabc12 <yogesh.chin@gmail.com>
2017-01-09 15:29:29 +0000

    TDD Test

---
+++ MedicalBilling/src/main/java/com/dev/poc/medicalbilling/util/MediHealthEnum.java
@@ -0,0 +1,9 @@
+package com.dev.poc.medicalbilling.util;
+
+public enum MediHealthEnum {
+	DAGNOSIS,
+	X_RAY,
+	BLOOD_TEST,
+	ECG,
+	VACCINE;
+}
--- MedicalBilling/src/test/java/com/dev/poc/medicalbillingtest/MedicalBillingTest.java
+++ MedicalBilling/src/test/java/com/dev/poc/medicalbillingtest/MedicalBillingTest.java
@@ -7,13 +7,14 @@ import org.junit.Test;
 import com.dev.poc.medicalbilling.GenerateMedicalBilling;
 import com.dev.poc.medicalbilling.impl.CartItem;
 import com.dev.poc.medicalbilling.impl.ShoppingCart;
+import com.dev.poc.medicalbilling.util.MediHealthEnum;
 
 public class MedicalBillingTest {
 
 	@Test
 	public void whenOneMediHealthServiceThanTotalCost() throws Exception {
 		ShoppingCart shoppingCart = new ShoppingCart();
-		shoppingCart.addCartItem(new CartItem("Diagnosis", 60.00, 1));
+		shoppingCart.addCartItem(new CartItem(MediHealthEnum.DAGNOSIS.name(), 60.00, 1));
 		shoppingCart.accept(new GenerateMedicalBilling());
 		assertEquals("Total Billing Cost",shoppingCart.getTotalBill(), 60.00, 0);
 	}
